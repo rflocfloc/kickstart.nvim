1690436aa73e66343e6308188dd9c4edf0de9898
rflocfloc <riccardoscandino@gmail.com>
2025-06-24 13:23:27 +0200

    updating nvim plugin

---
+++ lua/custom/plugins/ansible.lua
@@ -0,0 +1,15 @@
+return {
+  -- Definisce il plugin nvim-ansible
+  'pearofducks/nvim-ansible',
+  -- Specifica i tipi di file per cui questo plugin deve essere caricato.
+  -- LazyVim usa 'ft' per caricare i plugin solo quando necessario, ottimizzando l'avvio.
+  ft = { 'yaml.ansible', 'yaml' },
+
+  -- Qui puoi aggiungere opzioni specifiche per nvim-ansible, se ce ne sono.
+  -- Al momento, nvim-ansible non ha molte opzioni di configurazione direttamente esposte
+  -- tramite un metodo `setup()`. Funziona principalmente tramite il rilevamento dei filetype.
+  -- Tuttavia, puoi aggiungere una funzione `config` se in futuro avesse opzioni.
+  -- config = function()
+  --   -- Qui potresti chiamare una funzione di setup se il plugin la fornisse.
+  -- end,
+}
