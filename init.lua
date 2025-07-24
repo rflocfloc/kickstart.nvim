c2fb2d5c59eca2634e7104091e1feb05a331b0db
rflocfloc <riccardoscandino@gmail.com>
2025-07-24 14:16:27 +0200

    rose-pine colorscheme

--- init.lua
+++ init.lua
@@ -883,21 +883,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'rose-pine/neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    name = 'rose-pine',
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('gruvbox').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd 'colorscheme rose-pine-moon'
     end,
   },
 
